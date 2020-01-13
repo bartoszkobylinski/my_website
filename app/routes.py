a0599e3c5ba137a5c63d7275eaf1a6f58c90b80b
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2020-01-13 01:06:16 +0100

    changing pdf display to static files to avoid not displaying pdf on mobile phone

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,14 @@ from app.forms import ContactForm
 from flask_mail import Mail, Message
 from app.credentials import credentials
 
-mail = Mail()
-
-app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_SERVER"] = 'poczta.interia.pl'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_DEFAULT_SENDER"] = 'bartosz.kobylinski@gmail.com'
+app.config["MAIL_DEFAULT_SENDER"] = 'billboar@interia.pl'
 app.config["MAIL_USERNAME"] = credentials.get('username')
 app.config["MAIL_PASSWORD"] = credentials.get('password')
+
+mail = Mail()
 
 mail.init_app(app)
 
@@ -20,12 +20,12 @@ mail.init_app(app)
 def index():
     return render_template("index.html", title='Main Page')
 
-@app.route('/contact', methods=['GET','POST'],)
+@app.route('/contact', methods=['GET','POST'])
 def contact():
     form = ContactForm()
     if request.method == 'POST': 
         if form.validate():
-            message = Message(subject="New message has been sent through your website", recipients=['bartosz.kobylinski@gmail.com'])
+            message = Message(subject="New message has been sent through your website", recipients=['billboard@interia.pl'])
             message.body = f"From:{form.name.data} and from email {form.email.data} and message {form.message.data}."
             mail.send(message)
             return render_template("thanks.html")
@@ -45,7 +45,9 @@ def projects():
 @app.route('/thanks')
 def thanks():
     return render_template('thanks.html', title='Thank you')
-
+'''
 @app.route('/cv')
 def cv():
-    return render_template('cv.html', title='CV')
+    return render_template('cv.html', title='CV')\
+'''
+
