721d37acdefe67e27c52186b0d9c596663a69cf2
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2020-01-13 11:40:41 +0100

    Working with contact form

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,10 @@ from app.forms import ContactForm
 from flask_mail import Mail, Message
 from app.credentials import credentials
 
-app.config["MAIL_SERVER"] = 'poczta.interia.pl'
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_DEFAULT_SENDER"] = 'billboar@interia.pl'
+app.config["MAIL_DEFAULT_SENDER"] = 'bartosz.kobylinski@gmail.com'
 app.config["MAIL_USERNAME"] = credentials.get('username')
 app.config["MAIL_PASSWORD"] = credentials.get('password')
 
@@ -25,7 +25,7 @@ def contact():
     form = ContactForm()
     if request.method == 'POST': 
         if form.validate():
-            message = Message(subject="New message has been sent through your website", recipients=['billboard@interia.pl'])
+            message = Message(subject="New message has been sent through your website", recipients=['bartosz.kobylinski@gmail.com'])
             message.body = f"From:{form.name.data} and from email {form.email.data} and message {form.message.data}."
             mail.send(message)
             return render_template("thanks.html")
