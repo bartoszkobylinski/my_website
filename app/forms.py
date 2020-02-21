4c8cafea9ab7279b8a20ffffe534192f01bfbe83
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2020-02-21 23:00:04 +0100

    Creating layout from scratch, adding some validation in forms

--- app/forms.py
+++ app/forms.py
@@ -4,18 +4,18 @@ from wtforms.validators import InputRequired, Email, Regexp, Length
 
 class ContactForm(FlaskForm):
     name = StringField(
-        "Name", [
+        "Name *", [
         InputRequired("Please enter your name"),
         Length(min=1,max=35, message="You can provide name with max 35 characters")]
         )
     email = StringField(
-        "Email", validators=[
+        "Email *", validators=[
             InputRequired("Please enter your email"), 
             Email()
             ]
             )
     message = TextAreaField(
-        "Message", validators=[
+        "Message *", validators=[
             InputRequired("Please enter you message"),
             Length(min=1, max=200, message="Message can contain only 200 characters")
             ]
