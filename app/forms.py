e0ae84e54f309625edca0195f11107f637644533
bartek <bartosz.kobylinski@gmail.com>
2020-09-11 18:21:58 +0200

    ADDING A QUESTION FIELD IN THE CONTACT FORM, ADDING A FLAKE8
    - adding a question field with some calculation to avoid getting to much spam of bots
    
    - adding a flake8 linting library to update code to PEP

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField, StringField
-from wtforms.validators import InputRequired, Email, Regexp, Length
+from wtforms.validators import InputRequired, Email, Length
+
 
 class ContactForm(FlaskForm):
     name = StringField(
         "Name *", [
-        InputRequired("Please enter your name"),
-        Length(min=1,max=35, message="You can provide name with max 35 characters")]
-        )
+            InputRequired("Please enter your name"),
+            Length(min=1,
+                   max=35,
+                   message="You can provide name with max 35 characters")]
+                        )
     email = StringField(
         "Email *", validators=[
-            InputRequired("Please enter your email"), 
+            InputRequired("Please enter your email"),
             Email()
             ]
             )
     message = TextAreaField(
         "Message *", validators=[
             InputRequired("Please enter you message"),
-            Length(min=1, max=200, message="Message can contain only 200 characters")
+            Length(min=1,
+                   max=200,
+                   message="Message can contain only 200 characters")
             ]
             )
+    question = StringField("Please enter a result of 15 + 16 *",
+                           validators=[
+                                InputRequired(
+                                    "Please enter a result of 15+16"),
+                                Length(min=1, max=2)
+                                         ])
     submit = SubmitField("Send")
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, request, redirect
+from flask import render_template, request
 from app.forms import ContactForm
 from flask_mail import Mail, Message
 from app.credentials import credentials
@@ -15,32 +15,40 @@ mail = Mail()
 
 mail.init_app(app)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", title='Main Page')
 
-@app.route('/contact', methods=['GET','POST'])
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
-    if request.method == 'POST' and form.validate(): 
-        message = Message(subject="New message has been sent through your website", recipients=[credentials.get('username')])
-        message.body = f"From:{form.name.data} and from email {form.email.data} and message {form.message.data}."
-        mail.send(message)
+    question = "31"
+    if request.method == 'POST' and form.validate():
+        message = Message(
+                subject="New message has been sent through your website",
+                recipients=[credentials.get('username')])
+        message.body = f"""From:{form.name.data} and from email
+                        {form.email.data} and message {form.message.data}."""
+        if form.question.data == question:
+            mail.send(message)
         return render_template("thanks.html")
     else:
         return render_template("contact.html", title='Contact Page', form=form)
 
+
 @app.route('/about_me')
 def about_me():
     return render_template('about_me.html', title='About me')
-    
+
+
 @app.route('/projects')
 def projects():
     return render_template('projects.html', title='My projects')
 
+
 @app.route('/thanks')
 def thanks():
     return render_template('thanks.html', title='Thank you')
-
-
