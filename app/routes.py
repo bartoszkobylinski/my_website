95687d335cdaa3753ccae1aff65c1a3521e07901
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-06-28 19:39:22 +0200

    "Refactor: Remove email functionality from routes"
    Detailed message:
    This commit includes the removal of the email sending functionality from the app routes. The Flask-Mail extension and related configuration have been removed, along with the '/contact' route that used to handle sending emails. The application's dependencies have been simplified as a result, now only including Flask and its render_template function.

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,10 @@
 import logging
-from flask import Flask, render_template, request
-from forms import ContactForm
-from flask_mail import Mail, Message
-from credentials import credentials
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 logging.basicConfig(filename="my_website_logs.log", level=logging.WARNING)
-
-app.config["MAIL_SERVER"] = 'smtp.gmail.com'
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_DEFAULT_SENDER"] = credentials.get('username')
-app.config["MAIL_USERNAME"] = credentials.get('username')
-app.config["MAIL_PASSWORD"] = credentials.get('password')
-
-mail = Mail()
-
-mail.init_app(app)
 
 
 @app.route('/')
@@ -28,27 +15,6 @@ def main():
 @app.route('/index')
 def index():
     return render_template("index.html", title='Index Page')
-
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    form = ContactForm()
-    question = "Oslo"
-    if request.method == 'POST' and form.validate():
-        message = Message(
-            subject="New message has been sent through your website",
-            recipients=[credentials.get('username')])
-        message.body = f"""From:{form.name.data} and from email
-                        {form.email.data} and message {form.message.data}."""
-        if form.question.data == question:
-            try:
-                mail.send(message)
-                logging.info("Email sent successfully")
-            except Exception as error:
-                logging.error(f"Error occurred while sending email: {error}")
-        return render_template("thanks.html")
-    else:
-        return render_template("contact.html", title='Contact Page', form=form)
 
 
 @app.route('/about_me')
